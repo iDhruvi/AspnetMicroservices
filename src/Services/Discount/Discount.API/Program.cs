0996b415445e3fa818474ff153631b20a476da16
DhruviModi <dhruvimodi.dcs22n@vnsgu.ac.in>
2023-04-22 19:49:14 +0530

    Discount API implemented.

---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,30 @@
+using Discount.API.Extentions;
+using Discount.API.Repositories;
+
+//var host = Host.CreateDefaultBuilder(args).Build();
+//host.MigrateDatabase<Program>();
+//host.Run();
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+var app = builder.Build();
+app.MigrateDatabase<Program>();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
